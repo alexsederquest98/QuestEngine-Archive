1d516927828117099961e1bfb693422d38797bdb
Biggkahuna98 <alexsederquest98@gmail.com>
2023-10-09 11:27:03 -0400

    Working on logger

--- Quest/src/Core/Application.cpp
+++ Quest/src/Core/Application.cpp
@@ -1,6 +1,10 @@
 #include "qepch.h"
 #include "Application.h"
 
+#include <string>
+
+#include <iostream>
+
 namespace Quest
 {
 	bool Application::s_Instantiated = false;
@@ -9,6 +13,10 @@ namespace Quest
 	{
 		QE_CORE_ASSERT_MSG(!s_Instantiated, "Only 1 Application class may be used.");
 		s_Instantiated = true;
+
+		//std::string buffer = std::format("Hi {0}, How are your {1}?", "Bob", "toes");
+		//std::cout << buffer << std::endl;
+		
 
 		QE_CORE_INFO("Application Initialized");
 	}
