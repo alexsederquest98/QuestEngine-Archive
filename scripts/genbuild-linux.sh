a70e4d04ba783a8b9f864716d9c44d736e126c3c
Biggkahuna98 <alexsederquest98@gmail.com>
2023-10-18 14:03:35 -0400

    Added glm as a submodule (again...)

--- scripts/genbuild-linux.sh
+++ scripts/genbuild-linux.sh
@@ -1,1 +1,1 @@
-cmake -B build -S .. -DCMAKE_CXX_COMPILER=/usr/bin/clang++-17 -DCMAKE_C_COMPILER=/usr/bin/clang-17 -DCMAKE_LINKER=/usr/bin/lld-link-17
+cmake -B build -S . -DCMAKE_CXX_COMPILER=/usr/bin/clang++-17 -DCMAKE_C_COMPILER=/usr/bin/clang-17 -DCMAKE_LINKER=/usr/bin/lld-link-17
