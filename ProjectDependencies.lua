8f2e4b51305687d78bb2be501b26b3ef8c69f172
Alex Sederquest <alexsederquest98@gmail.com>
2023-10-31 18:05:21 -0400

    Removed premake5

--- ProjectDependencies.lua
+++
@@ -1,4 +0,0 @@
--- Include directories relative to the root (solution)
-Includes = {}
-Includes["spdlog"] = "%{wks.location}/Quest/vendor/spdlog/include"
-Includes["glfw"] = "%{wks.location}/Quest/vendor/glfw/include"
--- setup.bat
+++
@@ -1,4 +0,0 @@
-if not exist "build" mkdir "build"
-cd build
-../scripts/GenerateProjects.bat
-REM
